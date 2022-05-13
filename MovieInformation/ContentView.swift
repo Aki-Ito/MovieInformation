ce15abadb752a9e1093c3118de4f247a564d4bcd
Aki-Ito <60352606+Aki-Ito@users.noreply.github.com>
2022-05-13 20:54:56 +0900

    Initial Commit

---
+++ MovieInformation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieInformation
+//
+//  Created by 伊藤明孝 on 2022/05/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieInformation/MovieInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieInformationApp.swift
+//  MovieInformation
+//
+//  Created by 伊藤明孝 on 2022/05/13.
+//
+
+import SwiftUI
+
+@main
+struct MovieInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
